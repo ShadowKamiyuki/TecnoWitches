09b1be0fe481a80924e3bd584df4f42c61ff7926
Shadow <171745324+ShadowKamiyuki@users.noreply.github.com>
2025-11-07 21:59:44 -0300

    Add/UIManager
    added UIManager to control the player HUD

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviourSingleton<UIManager>
+{
+    [Header("Screens")]
+    public GameObject pauseScreen;
+    public GameObject resultScreen;
+
+    [Header("HUD elements")]
+    [SerializeField] private Image switchCooldown;
+    private Coroutine cooldownRoutine;
+
+    [Header("ASync Loader")]
+    [SerializeField] private ASyncLoader asyncLoader;
+
+    protected override void OnAwaken()
+    {
+        ServiceLocator.Register<UIManager>(this);
+        DisableScreens();
+    }
+
+    private void OnDestroy()
+    {
+        ServiceLocator.Unregister<UIManager>();
+    }
+
+    public void DisableScreens()
+    {
+        pauseScreen.SetActive(false);
+        resultScreen.SetActive(false);
+    }
+
+    public void DestroySingleton()
+    {
+        Destroy(UIManager.Instance.gameObject);
+    }
+
+    public void DisplayCooldown(float cooldown)
+    {
+        if (cooldownRoutine != null)
+            StopCoroutine(cooldownRoutine);
+
+        switchCooldown.fillAmount = 1f;
+        cooldownRoutine = StartCoroutine(Cooldown(cooldown));
+    }
+
+    private IEnumerator Cooldown(float cooldown)
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < cooldown)
+        {
+            timeElapsed += Time.deltaTime;
+            switchCooldown.fillAmount = Mathf.Clamp01(1f - (timeElapsed / cooldown));
+            yield return null;
+        }
+
+        switchCooldown.fillAmount = 0f;
+        cooldownRoutine = null;
+    }
+
+    #region OnActionButton
+
+    public void OnResumeGameClicked()
+    {
+        GameManager.Instance.SetGameState(GameManager.GameState.Gameplay);
+    }
+
+    public void OnMainMenuButtonClicked()
+    {
+        DestroySingleton();
+        asyncLoader.LoadLevelBtn("MainMenu");
+        GameManager.Instance.SetGameState(GameManager.GameState.MainMenu);
+    }
+
+    public void OnRestartButtonClicked()
+    {
+        DestroySingleton();
+        asyncLoader.LoadLevelBtn("GameScene");
+        GameManager.Instance.SetGameState(GameManager.GameState.Gameplay);
+        resultScreen.SetActive(false);
+    }
+
+    #endregion
+}
