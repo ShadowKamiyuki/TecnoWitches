3fc50e7417cce74c73d309faf6373ecae73fd697
Shadow <171745324+ShadowKamiyuki@users.noreply.github.com>
2025-11-07 22:00:33 -0300

    WIP/DimensionalSwitch
    still a WIP feature

---
+++ Assets/Scripts/RoomScripts/DimensionalSwitch/DimensionalSwitch.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class DimensionalSwitch : MonoBehaviour
+{
+    [Header("Room Settings")]
+    [SerializeField] private GameObject physicRoom;
+    [SerializeField] private GameObject digitalRoom;
+    [SerializeField] private float upDistanceMult;
+
+    [SerializeField] private GameObject player; // in the future with the generator, once the room is placed we need to get a ref to player.
+    private bool isInsideRoom;
+    private bool inDigitalWorld;
+
+    public DimensionalSwitch GetDimensionalSwitch()
+    {
+        return this;
+    }
+
+    public void CanSwitchDimension()
+    {
+        if (isInsideRoom)
+        {
+            SwitchDimension();
+        }
+    }
+
+    private void SwitchDimension()
+    {
+        if (!inDigitalWorld)
+        {
+            digitalRoom.SetActive(true);
+            physicRoom.SetActive(false);
+
+            player.transform.position = player.transform.position + Vector3.up * upDistanceMult;
+
+            inDigitalWorld = true;
+        }
+        else
+        {
+            digitalRoom.SetActive(false);
+            physicRoom.SetActive(true);
+
+            player.transform.position = player.transform.position + Vector3.down * upDistanceMult;
+
+            inDigitalWorld = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInsideRoom = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInsideRoom = false;
+        }
+    }
+}
