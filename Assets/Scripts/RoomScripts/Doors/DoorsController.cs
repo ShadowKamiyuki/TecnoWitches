2dcabad004e00fd190908e314da4c77a2d59c891
Pablo <171745324+ShadowKamiyuki@users.noreply.github.com>
2025-11-24 17:29:25 -0300

    Add/Doors-control
    Added a door control to avoid player escaping from a room that is not cleared. It still needs adjustments for animation, transparency alpha and more details

--- Assets/Scripts/RoomScripts/Doors/DoorsController.cs
+++ Assets/Scripts/RoomScripts/Doors/DoorsController.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorsController : MonoBehaviour
+public class DoorsController : MonoBehaviour, IUpdatable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] doors;
+
+    private CustomUpdateManager customUpdate;
+    private bool isInsideRoom;
+    private bool isRoomCleared;
+
+    private void OnEnable()
     {
-        
+        ServiceLocator.Get<CustomUpdateManager>().Register(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        customUpdate = ServiceLocator.Get<CustomUpdateManager>();
+        if (customUpdate != null)
+        {
+            customUpdate.Unregister(this);
+        }
+    }
+
+    public void Tick(float deltaTime)
+    {
+        if (isRoomCleared)
+        {
+            OpenDoors();
+        }
+    }
+
+    private void CloseDoors()
+    {
+        foreach (GameObject door in doors)
+        {
+            door.transform.position = new Vector3(door.transform.position.x, door.transform.position.y + 5, door.transform.position.z);
+        }
+    }
+
+    private void OpenDoors()
+    {
+        foreach (GameObject door in doors)
+        {
+            door.transform.position = new Vector3(door.transform.position.x, door.transform.position.y - 5, door.transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CloseDoors();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OpenDoors();
+        }
     }
 }
