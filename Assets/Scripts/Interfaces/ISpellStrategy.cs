feffd405c5d1d6ea182476aaad2fe2b721cc6396
Pablo <171745324+ShadowKamiyuki@users.noreply.github.com>
2025-12-13 15:03:10 -0300

    Delete/old-system
    Deleted or renamed some scripts of the old shooting system

--- Assets/Scripts/Interfaces/ISpellStrategy.cs
+++
@@ -1,4 +0,0 @@
-public interface ISpellStrategy
-{
-    public void Cast(SpellContext context);
-}
--- Assets/Scripts/SpellsScripts/SpellStrategy.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class SpellStrategy : ScriptableObject, ISpellStrategy
-{
-    public abstract void Cast(SpellContext context);
-}
