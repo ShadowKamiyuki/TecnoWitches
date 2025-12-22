39e954dc3855c3c66254a0392748206661b4ff6a
Pablo <171745324+ShadowKamiyuki@users.noreply.github.com>
2025-12-22 19:28:08 -0300

    Update/basic-manager
    added the logic of a basic manager, gonna upgrade it for an audio mixer

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,18 +1,71 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : MonoBehaviourSingleton<AudioManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("Clips")]
+    [SerializeField] private List<AudioClip> musicClips;
+    [SerializeField] private List<AudioClip> sfxClips;
+
+    private Dictionary<string, AudioClip> musicDictionary;
+    private Dictionary<string, AudioClip> sfxDictionary;
+
+    protected override void OnAwaken()
     {
-        
+        Debug.Log("AudioManager inicializado");
+
+        musicDictionary = new Dictionary<string, AudioClip>();
+
+        foreach (AudioClip clip in musicClips)
+        {
+            musicDictionary[clip.name] = clip;
+        }
+
+        sfxDictionary = new Dictionary<string, AudioClip>();
+
+        foreach (var clip in sfxClips)
+        {
+            sfxDictionary[clip.name] = clip;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayMusic(string clipName, bool loop = true)
     {
-        
+        if (musicDictionary.TryGetValue(clipName, out AudioClip clip))
+        {
+            musicSource.clip = clip;
+            musicSource.loop = loop;
+            musicSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"{clipName} not found");
+        }
+    }
+
+    public void PlaySFX(string clipName)
+    {
+        if (sfxDictionary.TryGetValue(clipName, out AudioClip clip))
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogWarning($"{clipName} not found");
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
     }
 }
